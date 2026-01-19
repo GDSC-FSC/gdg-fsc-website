878f8b70d8132352f35a4d5aa02b7982cc6bd95c
WomB0ComB0 <mike@mikeodnis.dev>
2026-01-19 15:47:08 -0500

    chore(config): 🔧 Disable PWA plugin configuration
    Comment out the `vite-plugin-pwa` import and configuration block in `vite.config.ts`.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-import path from 'node:path';
 import { vCache } from '@raegen/vite-plugin-vitest-cache';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'node:path';
 import sharp from 'sharp';
 import { defineConfig, loadEnv } from 'vite';
 import { imagetools } from 'vite-imagetools';
@@ -26,7 +26,7 @@ import { comlink } from 'vite-plugin-comlink';
 import csp from 'vite-plugin-csp-guard';
 import Inspect from 'vite-plugin-inspect';
 import lqip from 'vite-plugin-lqip';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { logger } from '../../packages/shared/utils/src';
 
@@ -207,22 +207,22 @@ export default defineConfig(({ mode }) => {
       /**
        * @see https://www.npmjs.com/package/vite-plugin-pwa
        */
-      VitePWA({
-        workbox: {
-          cleanupOutdatedCaches: true,
-          skipWaiting: true,
-          clientsClaim: true,
-          globPatterns: ['**/*'],
-          globIgnores: ['**/OFL.txt', '**/*.map'],
-          maximumFileSizeToCacheInBytes: (1_024 * 2) ** 2, //
-        },
-        registerType: 'autoUpdate',
-        injectRegister: 'auto',
-        includeAssets: ['**/*'],
-        devOptions: {
-          enabled: false,
-        },
-      }),
+      // VitePWA({
+      //   workbox: {
+      //     cleanupOutdatedCaches: true,
+      //     skipWaiting: true,
+      //     clientsClaim: true,
+      //     globPatterns: ['**/*'],
+      //     globIgnores: ['**/OFL.txt', '**/*.map'],
+      //     maximumFileSizeToCacheInBytes: (1_024 * 2) ** 2, //
+      //   },
+      //   registerType: 'autoUpdate',
+      //   injectRegister: 'auto',
+      //   includeAssets: ['**/*'],
+      //   devOptions: {
+      //     enabled: false,
+      //   },
+      // }),
       tailwindcss(),
     ],
     server: {
